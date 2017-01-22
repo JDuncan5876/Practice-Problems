80372ff71f0b18ea0f25147fc2759d89bcf268b5
JDuncan45 <JDuncan5876@gmail.com>
2017-01-21 20:08:45 -0500

    Find length of last word in a string

---
+++ LengthOfLastWord.java
@@ -0,0 +1,16 @@
+/**
+ * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+ *
+ * If the last word does not exist, return 0.
+ *
+ * @author Jared Duncan
+ */
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        String[] strings = s.split(" ");
+        if (strings.length == 0) {
+            return 0;
+        }
+        return strings[strings.length - 1].length();
+    }
+}
