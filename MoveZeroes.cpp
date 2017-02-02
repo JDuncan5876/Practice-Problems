998280a0fb60d7cdb2bf22938e6a12b19b6ffb4d
JDuncan45 <JDuncan5876@gmail.com>
2017-02-02 01:26:28 -0500

    Move zeroes to back of list, this time in c++

---
+++ MoveZeroes.cpp
@@ -0,0 +1,15 @@
+class MoveZeroes {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        for (int j = 0; j < nums.size(); ++j) {
+            if (nums[j] != 0) {
+                nums[i++] = nums[j];
+            }
+        }
+
+        while (i < nums.size()) {
+            nums[i++] = 0;
+        }
+    }
+};
